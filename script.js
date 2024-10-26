41091642b04647ac8be08a7f834c4ee2e02af52c
MahtabAshfi <mahtabashfi1625@gmail.com>
2024-10-26 23:09:17 +0600

    cursor animation added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let cursor = document.getElementById("cursor");
+let cursorBlur = document.getElementById("cursor-blur");
+
+document.addEventListener("mousemove", function(e){
+  cursor.style.left = e.x + "px";
+  cursor.style.top = e.y + "px";
+  cursorBlur.style.left = e.x - 250 + "px";
+  cursorBlur.style.top = e.y - 250 + "px";
+})
+
 gsap.to("#nav",{
   backgroundColor:"#000",
-  duration:0.5,
   height:"105px",
   scrollTrigger:{
     trigger:"#nav",
@@ -8,5 +17,16 @@ gsap.to("#nav",{
     start:"top -10%",
     end:"top -11%",
     scrub:1
+  }
+})
+
+gsap.to("#main",{
+  backgroundColor:"#000",
+  scrollTrigger:{
+    trigger:"#main",
+    scroller:"body",
+    start:"top -65%",
+    end:"top -70%",
+    scrub:2
   }
 })
