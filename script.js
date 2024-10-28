97dcb0a32353def1f52b251dcaba436c158b847a
MahtabAshfi <mahtabashfi1625@gmail.com>
2024-10-28 23:51:04 +0600

    footer info added

--- script.js
+++ script.js
@@ -81,14 +81,13 @@ gsap.from("#colon2", {
   },
 });
 
-// gsap.from("#page4 h1", {
-//   y: 50,
-//   scrollTrigger: {
-//     trigger: "#page4 h1",
-//     scroller: "body",
-//     // markers:true,
-//     start: "top 75%",
-//     end: "top 70%",
-//     scrub: 3,
-//   },
-// });
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
